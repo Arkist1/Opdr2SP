eae1ce7f0a3668d88b8830203c624128d3a0553c
Arkist <masteroffwuju@gmail.com>
2022-03-10 14:38:38 +0100

    basis layout gemaakt

--- Main.py
+++ Main.py
@@ -1,6 +1,17 @@
 from pymongo import MongoClient
+import psycopg2
 
-client = MongoClient("localhost")
-db = client["test_database"]
 
-productcol = db['product_data']
+def updatedata():
+    client = MongoClient("localhost")
+    db = client["test_database"]
+    
+    productcol = db['product_data']
+    sessioncol = db['anonymous_sessions']
+    profilescol = db['anonymous_profiles']
+    
+    
+
+
+if __name__ == '__main__':
+    updatedata()
