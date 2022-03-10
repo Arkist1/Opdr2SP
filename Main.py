a7af7502fb54e5adb80b1dd8b71ba67b78260efa
Arkist <masteroffwuju@gmail.com>
2022-03-10 15:39:53 +0100

    postgres connect

--- Main.py
+++ Main.py
@@ -2,7 +2,19 @@ from pymongo import MongoClient
 import psycopg2
 
 
+def connectpostgres(dbname):
+    conn = psycopg2.connect(
+        host="localhost",
+        dbname=dbname,
+        user="postgres",
+        password="admin"
+    )
+    
+    return conn
+
+
 def updatedata():
+    postgres = psycopg2.connect("dbname=RecommendationEngine password=admin")
     client = MongoClient("localhost")
     db = client["test_database"]
     
