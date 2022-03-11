bdcda364f893661165b83fe737799ea581e4f1ba
Arkist <masteroffwuju@gmail.com>
2022-03-11 14:44:27 +0100

    data analyse

---
+++ data_analyse/profile.py
@@ -0,0 +1,23 @@
+{
+   "_id":{
+      "$oid":"5fd0d8c9701de4624db04b82"
+   },
+   "buids":[ # hier staan alle sessions in van 1 profile
+      "SF1.6.59-d266c7ed-a911-45e4-a3f5-83f76248df05" # dit is een sessie
+   ],
+   "sm":{
+      "created":{
+         "$date":"2020-12-09T14:01:45.142+0000"
+      },
+      "created_by":"LegacyDao" # hoe het profiel is aangemaakt
+   },
+   "meta":{
+      "has_device":true, # niet nuttig
+      "has_email":false, # niet nuttig
+      "has_utm_hash":false, # niet nuttig
+      "random":0.4351745417327053 # zeker niet nuttig
+   },
+   "latest_activity":{
+      "$date":"2020-12-09T14:01:40.392+0000"
+   } # niet nuttig
+}
