32065343b931222e5c18023d28c098d9756d0cbe
Arkist <masteroffwuju@gmail.com>
2022-03-13 12:54:58 +0100

    small changes

--- Main.py
+++ Main.py
@@ -1,19 +1,7 @@
 from Database import Database
 
-def updatedata(db):
-
-    dbname = db.name
-    print ("Connected tot", dbname)
-    # db = client["test_database"]
-    
-    products = db['product_data']
-    for product in products.find():
-        print(product)
-    
-    # sessioncol = db['anonymous_sessions']
-    # profilescol = db['anonymous_profiles']
 
 if __name__ == '__main__':
     database = Database("RecommendationEngine")
     
-    updatedata(database.mongodb)
+    database.updatedata()
